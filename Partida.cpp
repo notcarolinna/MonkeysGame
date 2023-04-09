4457ee05963d484ff5290f37a37109daff07f0a0
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-04-09 00:06:08 -0300

    Create Partida.cpp

---
+++ Partida.cpp
@@ -0,0 +1,36 @@
+#include "Partida.h"
+#include <iostream>
+#include "Partida.h"
+#include <vector>
+#include <algorithm>
+
+Partida::Partida(Macaquinho** macaquinhos, Reader reader) : macaquinhos(macaquinhos), reader(reader) {}
+
+Macaquinho* Partida::Vencedor() {
+	int n = reader.getTotalMacaquinhos();
+	std::vector<Macaquinho*> macaquinhosVec(macaquinhos, macaquinhos + n);
+
+	std::sort(macaquinhosVec.begin(), macaquinhosVec.end(), [](Macaquinho* a, Macaquinho* b) {
+		return a->getCocos() < b->getCocos();
+		});
+
+	return macaquinhosVec.back();
+}
+
+void Partida::IniciarPartida() {
+	int numRodadas = reader.getRodadas();
+
+	for (int i = 0; i < numRodadas; i++)
+	{
+		for (int j = 0; j < reader.getTotalMacaquinhos(); j++)
+		{
+			Macaquinho* macaquinho = macaquinhos[j];
+
+			macaquinhos[macaquinho->getMacacoPar()]->addCocosPares(macaquinho->getCocosPar());
+			macaquinho->addCocosPares(-1 * macaquinho->getCocosPar());
+
+			macaquinhos[macaquinho->getMacacoImpar()]->addCocosImpares(macaquinho->getCocosImpar());
+			macaquinho->addCocosImpares(-1 * macaquinho->getCocosImpar());
+		}
+	}
+}
